ec93a574037b05869fed8a12acd56136821ebc7a
Mitchell Foote <mcfoote321@gmail.com>
2022-08-04 17:03:07 -0400

    adding comments.

--- Develop/script.js
+++ Develop/script.js
@@ -17,22 +17,29 @@ generateBtn.addEventListener("click", writePassword);
 
 function generatePassword () {
 
+  //Define arrays for uppercase, lowercase, numbers, and symbol characters
   var upperArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
   "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   var lowerArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", 
   "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   var numberArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var symbolArray = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+  var symbolArray = ["!","@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+",
+  "<", ">", "`", "~", ";", ":", "?", "[", "]", "{", "}", "."];
+
+  //Define empty arrays for calculations and output
   var userArray = [];
   var resultArray = [];
   
-
+  //Take user input for number of characters
   var numChar = prompt ("Input desired number of characters for password within range 8 - 128");
+
+  // click yes/no for each category of characters
   var upper = confirm("Do you require Uppercase letters for your password?");
   var lower = confirm("Do you require Lowercase letters for your password?");
   var numbers = confirm("Do you require numbers for your password?");
   var symbols = confirm("Do you require special characters for your password?");
 
+  //Check for each category if chosen then concatonate to result array
   if (upper){
 
     resultArray = resultArray.concat(upperArray);
